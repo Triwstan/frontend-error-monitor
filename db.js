68fe274431386267cc2664771d0a0023a53aedd8
dosaida <tominotristan@gmail.com>
2026-01-19 13:12:53 +0800

    Implement IndexedDB setup in db.js

---
+++ db.js
@@ -0,0 +1,19 @@
+const DB_NAME = "error-monitor-db";
+const DB_VERSION = 1;
+const STORE_NAME = "errors";
+
+export function openDB() {
+  return new Promise((resolve, reject) => {
+    const request = indexedDB.open(DB_NAME, DB_VERSION);
+
+    request.onupgradeneeded = () => {
+      const db = request.result;
+      if (!db.objectStoreNames.contains(STORE_NAME)) {
+        db.createObjectStore(STORE_NAME, { keyPath: "id" });
+      }
+    };
+
+    request.onsuccess = () => resolve(request.result);
+    request.onerror = () => reject(request.error);
+  });
+}
